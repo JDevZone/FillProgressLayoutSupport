c831ccd44e046451b190264f4e7ab583f1ae8bde
jdevzone <jdevzone@gmail.com>
2019-09-12 17:20:16 +0530

    alternative of v0.0.2(AndroidX version)

---
+++ app/src/main/java/com/devzone/fpl_sample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.devzone.fpl_sample
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isFilled = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun toggleFill(view: View) {
+        val button: Button = view as Button
+        button.isEnabled = false
+
+        isFilled = !isFilled
+        fillL.setProgress(if (isFilled) 100 else 0)
+        fillB.setProgress(if (isFilled) 100 else 0)
+        fillB.setDoOnProgressEnd { v ->
+            button.isEnabled = true;button.text = if (isFilled) "Unfill" else "Fill"
+        }
+
+    }
+}
